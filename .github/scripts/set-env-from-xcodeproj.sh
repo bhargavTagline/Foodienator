2e780447c6d3e18bb611b364dcafde9c775154d4
bhargavTagline <bhargav.tagline@gmail.com>
2022-09-08 14:48:11 +0530

    CHECK ENV STEP

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 cd ios
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
+echo "$SHEME"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
  
